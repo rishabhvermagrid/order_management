d323d04b2e2e48c5250a0a4b1e9cb8f60105eeb7
rishabhvermagrid <rverma@griddynamics.com>
2026-06-21 23:00:36 +0530

    swagger

--- src/main/java/com/rishabh/order_management/CustomValidationAnnotation/StrongPassword.java
+++ src/main/java/com/rishabh/order_management/CustomValidationAnnotation/StrongPassword.java
@@ -5,7 +5,7 @@ import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
-@Documented//Makes annotation visible in generated javaDocs
+@Documented//Makes annotation visible in generated javaDocs, Tools in menu bar -> generate doc
 @Constraint(validatedBy = StrongPasswordValidator.class)//Use this validator class for validation logic.
 @Target(ElementType.FIELD)//Defines where annotations can be used, (method,parameter,type)
 @Retention(RetentionPolicy.RUNTIME)//keeps annotation available during runtime, without this: Spring can't read annotation while application runs
@@ -15,7 +15,9 @@ public @interface StrongPassword {
             "Password must contain uppercase, lowercase, number, special character and minimum 8 characters";
     //default validation error message
 
-    Class<?>[] groups() default {};
+    Class<?>[] groups() default {};//Used to apply validations conditionally for different operations
+    //like Create, Update,
 
-    Class<? extends Payload>[] payload() default {};
+    Class<? extends Payload>[] payload() default {};//used to attach metadata to validation errors, allows adding extra information , severity level : error category :
+    //warning vs critical
 }
